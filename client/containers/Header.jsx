f7b29edbd2fdc694f93d17376ad863feaac8a522
Jason Won <jason.j.won@gmail.com>
2016-03-02 01:22:40 -0800

    replace nav links with real routes

--- client/containers/Header.jsx
+++ client/containers/Header.jsx
@@ -12,17 +12,17 @@ export default class Header extends Component {
     return (
       <header>
         <ul className="dropdown-content" id="mobile-nav-dropdown">
-          <li><a href="sass.html">Transactions</a></li>
-          <li><a href="badges.html">Goals</a></li>
-          <li><a href="collapsible.html">Profile</a></li>
+          <li><a href="/transactions">Transactions</a></li>
+          <li><a href="/goals">Goals</a></li>
+          <li><a href="/profile">Profile</a></li>
         </ul>
         <nav className="teal accent-4">
           <div className="nav-wrapper container">
             <a href="#!" className="brand-logo">$$$</a>
             <ul className="right hide-on-med-and-down">
-              <li><a href="sass.html">Transactions</a></li>
-              <li><a href="badges.html">Goals</a></li>
-              <li><a href="collapsible.html">Profile</a></li>
+              <li><a href="/transactions">Transactions</a></li>
+              <li><a href="/goals">Goals</a></li>
+              <li><a href="/profile">Profile</a></li>
             </ul>
             <a href="#" data-activates="mobile-nav-dropdown" className="dropdown-button right hide-on-large-only"><i className="material-icons">menu</i></a>
           </div>
