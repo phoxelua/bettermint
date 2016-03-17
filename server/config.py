e725b5f0a5355e023f284cf0a4e875ceed30d4d7
Jason Won <jason.j.won@gmail.com>
2016-03-17 00:32:49 -0700

    add some utils, power up the token api, and fix some flake8 weirdness

--- server/config.py
+++ server/config.py
@@ -1,7 +1,8 @@
-SQLALCHEMY_DATABASE_URI = "postgresql://localhost"
+SQLALCHEMY_DATABASE_URI = 'postgresql://localhost'
 DEBUG = True
 HOST = '127.0.0.1'
 PORT = 3001
+SECRET_KEY = 'bettermint is cool'
 
 SHELLPLUS_POST_IMPORTS = {
     'server.models': 'Kitten'
