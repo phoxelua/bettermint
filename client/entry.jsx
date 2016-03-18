8c7480062409d212c64d66125ec44ef8e40e7305
Jason Won <jason.j.won@gmail.com>
2016-03-17 22:27:05 -0700

    add redux devtools

--- client/entry.jsx
+++ client/entry.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import { Provider } from 'react-redux';
 import { syncHistoryWithStore, routerReducer, replace } from 'react-router-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import createLogger from 'redux-logger';
 import promise from 'redux-promise';
 import thunk from 'redux-thunk';
@@ -14,10 +14,13 @@ import reducers from 'redux/reducers';
 import 'styles/sass/main.scss';
 
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunk,
-  promise,
-  createLogger()
+const createStoreWithMiddleware = compose(
+  applyMiddleware(
+    thunk,
+    promise,
+    createLogger()
+  ),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
 )(createStore);
 
 const store = createStoreWithMiddleware(reducers);
