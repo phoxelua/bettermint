149258ffcd809eea7e9f25e5a597fb8ae205656c
Jason Won <jason.j.won@gmail.com>
2016-05-01 17:54:58 -0700

    separate debit and credit transactions

--- client/components/Transactions.jsx
+++ client/components/Transactions.jsx
@@ -11,14 +11,22 @@ const Transactions = (props) => {
           <colgroup>
             <col span="1" style={{ width: 20 + '%' }}></col>
             <col span="1" style={{ width: 40 + '%' }}></col>
-            <col span="1" style={{ width: 40 + '%' }}></col>
+            <col span="1" style={{ width: 20 + '%' }}></col>
+            <col span="1" style={{ width: 20 + '%' }}></col>
           </colgroup>
           <tbody>
+            <tr>
+                <th>Date</th>
+                <th>Name</th>
+                <th>Debit</th>
+                <th>Credit</th>
+              </tr>
             {props.transactions.map(transaction => (
               <tr key={transaction._id}>
                 <td>{transaction.date}</td>
                 <td>{transaction.name}</td>
-                <td>{transaction.amount.toFixed(2)}</td>
+                <td>{transaction.amount.toFixed(2) >= 0 ? transaction.amount.toFixed(2) : ''}</td>
+                <td>{transaction.amount.toFixed(2) < 0 ? (-transaction.amount).toFixed(2) : ''}</td>
               </tr>
             ))}
           </tbody>
