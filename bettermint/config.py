840a4e1f44d771e12fda04474ec8ed2ec2490dc5
phoxelua <howardanguyen@gmail.com>
2016-05-01 17:54:20 -0700

    Removed config pls

--- bettermint/config.py
+++
@@ -1,9 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://localhost'
-DEBUG = True
-HOST = '127.0.0.1'
-PORT = 3001
-SECRET_KEY = 'bettermint is cool'
-
-# Plaid
-PLAID_CLIENT_ID = '56cd5d67d7a9b3f150271ad4'
-PLAID_SECRET = '8c1637afb55d3db46dba3270317c7f'
